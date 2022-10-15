c9341de1a3ce81795436ba74eca1a5db69ff1741
Ifeanyi Ani <ifeanyiani155@gmail.com>
2022-10-15 18:29:16 +0100

    admin

--- js/dump.js
+++ js/dump.js
@@ -8,3 +8,16 @@
 // </tr>`
 //   };
 // dasboardProductView.html(quickView);
+
+  //  let img=URL.createObjectURL(evt.target.files[0]);
+  //  imgPreview.attr("src",img)
+    // const file=this.files[0];
+    // console.log(file);
+    // if(file){
+    //   let reader= new FileReader();
+    //   reader.onload=function (event){
+    //     console.log(event.target.result);
+    //     imgPreview.attr("src",event.target.result);
+    //   }
+    //   reader.readAsDataURL(file);
+    // }
--- js/main.js
+++ js/main.js
@@ -14,39 +14,36 @@ let productQuantity =$("#productQuantity");
 let productQuantityError =$("#productQuantityError");
 let productDiscription =$("#productDiscription");
 let productDiscriptionError =$("#productDiscriptionError");
-let productSample =$("#productSample");
+let productSamp =$("#productSample");
 let imgPreview=$("#imgPreview");
 let productSampleError =$("#productSampleError");
 
 let productsView=$("#productsView");
 let addProduct=$("#addProBtn");
 
+let userCard=$("#userCard");
+
+let dasboardProductView=$("#quickView");
+let dasboardUserView=$(".dashboardUserView");
 
 
 let productDetails=[];
 let prodcutCatData=["choose category", "Originals", "Fall Favorite","Bestsellers","cleansers & Moistures"]
-let editIndex;
+let editedIndex;
+
+
+let userDetails=[];
+let userDetailsDashboard=[];
+let productDetailsDashboard=[];
+dashboardProduct();
+dashboardUser();
 displayProduct();
+displayUser();
 
-$(document).ready(()=>{
-  productSample.change(function(evt){
-   let img=URL.createObjectURL(evt.target.files[0]);
-   imgPreview.attr("src",img)
-    // const file=this.files[0];
-    // console.log(file);
-    // if(file){
-    //   let reader= new FileReader();
-    //   reader.onload=function (event){
-    //     console.log(event.target.result);
-    //     imgPreview.attr("src",event.target.result);
-    //   }
-    //   reader.readAsDataURL(file);
-    // }
-    localStorage.setItem("product", JSON.stringify(productDetails));
-  })
-  displayProduct();
-})
-
+productSamp.on("change", showImg)
+function showImg(){
+  imgPreview.attr("src", productSamp.val())
+}
 // siden nav
 function activeLink() {
   lists.forEach((item) => {
@@ -57,7 +54,6 @@ function activeLink() {
 }
 lists.forEach((item) => item.addEventListener("mouseover", activeLink));
 
-
 toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
@@ -65,7 +61,8 @@ toggle.onclick = function () {
 
 
 // populating product category drop down
-window.onload=categoryPopulate();
+
+
 function categoryPopulate(){
   let prdCategory=document.getElementById("productCategory")
   prdCategory.innerHTML="";
@@ -81,6 +78,7 @@ function categoryPopulate(){
 addProductContainer.hide();
 $("#newProduct").on("click",()=>{
   addProductContainer.show();
+  categoryPopulate();
 })
 $("#close").on("click",()=>{
   addProductContainer.hide();
@@ -98,44 +96,44 @@ function validateProductForm(){
     productNameError.html("");
     productName.css("outline","none");
   }
-  if(productCategory.val()==""&&!isNaN(productCategory.val())){
+  if(productCategory.val()==""){
     productCategoryError.html("<i>please enter the product name</i>");
     productCategory.css("outline","3px solid red");
   }else{
     productCategoryError.html("");
     productCategory.css("outline","none");
   }
-  if(productPrice.val()==""&&!isNaN(productPrice.val())){
+  if(productPrice.val()==""){
     productPriceError.html("<i>please enter the product name</i>");
     productPrice.css("outline","3px solid red");
   }else{
     productPriceError.html("");
     productPrice.css("outline","none");
   }
-  if(productQuantity.val()==""&&!isNaN(productQuantity.val())){
+  if(productQuantity.val()==""){
     productQuantityError.html("<i>please enter the product name</i>");
     productQuantity.css("outline","3px solid red");
   }else{
     productQuantityError.html("");
     productQuantity.css("outline","none");
   }
-  if(productDiscription.val()==""&&!isNaN(productDiscription.val())){
+  if(productDiscription.val()==""){
     productDiscriptionError.html("<i>please enter the product name</i>");
     productDiscription.css("outline","3px solid red");
   }else{
     productDiscriptionError.html("");
     productDiscription.css("outline","none");
   }
-  if(productSample.val()==""&&!isNaN(productSample.val())){
+  if(productSamp.val()==""){
     productSampleError.html("<i>please enter the product name</i>");
     productSample.css("outline","3px solid red");
   }else{
     productSampleError.html("");
-    productSample.css("outline","none");
+    productSamp.css("outline","none");
   }
 
-if(productName.val()!=""&&productCategory.val()!="" && productPrice.val()!=""&&productQuantity.val()!=""&&productDiscription.val()!=""&&productSample.val()!=""){
-  if(editIndex !=null){
+if(productName.val()!=""&&productCategory.val()!="" && productPrice.val()!=""&&productQuantity.val()!=""&&productDiscription.val()!=""&&productSamp.val()!=""){
+  if(editedIndex !=null){
     updateProduct();
   }else{
     createProduct();
@@ -144,82 +142,258 @@ if(productName.val()!=""&&productCategory.val()!="" && productPrice.val()!=""&&p
 
 }
 function createProduct(){
-productObj={
-  "image": imgPreview.attr("src"),
+let productObj={
+  "image": productSamp.val(),
   "name": productName.val(),
   "category": productCategory.val(),
   "price": productPrice.val(),
   "quantity": productQuantity.val(),
   "description": productDiscription.val(),
 };
-productDetails.push(productObj);
-localStorage.setItem("product", JSON.stringify(productDetails));
+$.ajax({
+  type:"post",
+  url:"http://159.65.21.42:9000/create/product",
+  data:productObj,
+  success:function(response){
+    console.log(response);
 
+    if(response["error"]){
+      alert(response["error"])
+    }else{
+      alert(`${response["name"]} product added`)
+    }
+
+  },
+  error:function(error){
+    console.log(error);
+    alert(error.statusText)
+  }
+})
 displayProduct();
 addProductContainer.hide();
 }
+
 function displayProduct(){
-  let productStorage=localStorage.getItem("product");
-  if(productStorage !=null){
-    productDetails=JSON.parse(productStorage);
-  }
-  let productView="";
-  for(let i=0;i<productDetails.length;i++){
-    productView+=` <tr>
-<td>${i+1}</td>
-<td>${productDetails[i]["name"]}</td>
-<td class="mobi-disabled">${productDetails[i]["description"]}</td>
-<td class="mobi2-disabled">
-    <div class="img-con"><img src=${productDetails[i]["image"]} alt="" id="imgDis"></div>
-</td>  
-<td>$${productDetails[i]["price"]}</td>
-<td>${productDetails[i]["category"]}</td>
-<td class="mobi-disabled">${productDetails[i]["quantity"]}</td>
-<td><a href="#" class="edit-btn" index="${i}"><ion-icon name="pencil-outline"></ion-icon></a></td>
-<td><a href="#" class="delete-btn" index="${i}"><ion-icon name="trash-outline"></ion-icon></a></td>
-</tr>`
-
-  }
-  productsView.html(productView);
-
+  productsView.html(" <h2>Loading data ..........</h2>")
+  $.ajax({
+    type:"get",
+    url:"http://159.65.21.42:9000/products",
+    success:function(response){
+      console.log(response);
+      productDetails=response
+      productDetails=productDetails.reverse()
+      $(".productItems").text(`${productDetails.length}`)
+      let productView="";
+      for(let i=0;i<productDetails.length;i++){
+        productView+=` <tr>
+    <td>${i+1}</td>
+    <td>${productDetails[i]["name"]}</td>
+    <td class="mobi-disabled">${productDetails[i]["description"]}</td>
+    <td class="mobi2-disabled">
+        <div class="img-con"><img src="${productDetails[i]["image"]}" alt="" id="imgDis"></div>
+    </td>  
+    <td>$${productDetails[i]["price"]}</td>
+    <td>${productDetails[i]["category"]}</td>
+    <td class="mobi-disabled">${productDetails[i]["quantity"]}</td>
+    <td><a href="#" class="edit-btn" index="${i}"><ion-icon name="pencil-outline"></ion-icon></a></td>
+    <td><a href="#" class="delete-btn" index="${i}"><ion-icon name="trash-outline"></ion-icon></a></td>
+    </tr>`
+    
+      }
+      productsView.html(productView);
+    },
+    error:function(error){
+      alert(error.statusText)
+    }
+  })
 }
 function deleteProduct(){
   let i=$(this).attr("index");
-  productDetails.splice(i,1);
-  localStorage.setItem("product",JSON.stringify(productDetails));
-  displayProduct();
+  $.ajax({
+    type:"delete",
+    url:"http://159.65.21.42:9000/product/"+productDetails[i]["_id"],
+    success:function(response){
+      console.log(response);
+  
+      if(response["success"]){
+        alert(`${response["success"]}`)
+        //window.location.href=""
+        displayProduct();
+      }
+    },
+    error:function(error){
+      console.log(error);
+      alert(error.statusText)
+    }
+  })
 }
 function editProduct(){
-  editIndex=$(this).attr("index");
+  editedIndex=$(this).attr("index");
   addProductContainer.show();
   addProduct.text("UPDATE");
+  categoryPopulate();
 
-
-productSample.val(productDetails[editIndex]["image"]);
-productName.val(productDetails[editIndex]["name"]);
-productCategory.val(productDetails[editIndex]["category"]);
-productPrice.val(productDetails[editIndex]["price"])
-productQuantity.val(productDetails[editIndex]["quantity"]);
-productDiscription.val(productDetails[editIndex]["description"]);
-
-addProductContainer.hide();
+productSamp.val(productDetails[editedIndex]["image"]);
+productName.val(productDetails[editedIndex]["name"]);
+productCategory.val(productDetails[editedIndex]["category"]);
+productPrice.val(productDetails[editedIndex]["price"])
+productQuantity.val(productDetails[editedIndex]["quantity"]);
+productDiscription.val(productDetails[editedIndex]["description"]);
 
 }
 function updateProduct(){
-  editedProduct={
-    "image": imgPreview.attr("src"),
+ let reviewProduct={
+    "image": productSamp.val(),
+    "product_id":"5ee8c74752307a08249b2970",
     "name": productName.val(),
     "category": productCategory.val(),
     "price": productPrice.val(),
     "quantity": productQuantity.val(),
     "description": productDiscription.val(),
   };
-  productDetails[editIndex]=(editedProduct);
-  localStorage.setItem("product",JSON.stringify(productDetails));
-  displayProduct();
-  addProductContainer.hide();
-addProduct.text("ADD PRODUCT")
-  editIndex=null;
+  $.ajax({
+    type:"put",
+    url:"http://159.65.21.42:9000/update/product"+productDetails[editedIndex],
+    data:reviewProduct,
+    success:function(response){
+      console.log(response);
+  
+      if(response["error"]){
+        alert(response["error"])
+      }else{
+        alert(`${response["name"]} successfully updated`)
+        displayProduct();
+       addProductContainer.hide();
+      addProduct.text("ADD PRODUCT")
+      editedIndex=null;
+      }
+  
+    },
+    error:function(error){
+      console.log(error);
+      alert(error.statusText)
+    }
+  })
   
 }
+
+// for managing user
+
+function displayUser(){
+
+  userCard.html(" <h2>Loading data ..........</h2>")
+  $.ajax({
+    type:"get",
+    url:"http://159.65.21.42:9000/users",
+    success:function(response){
+      userDetails=response
+      userDetails=userDetails.reverse()
+      $(".userNo").text(`${userDetails.length}`)
+      let userView="";
+      for(let i=0;i<userDetails.length;i++){
+        userView+=` <div class="user-card">
+        <div class="img-container">
+            <img src="images/profile.jpg" alt="">
+        </div>
+    
+        <div class="details">
+            <div class="name">${userDetails[i]["name"]}</div>
+            <div class="email">${userDetails[i]["email"]}</div>
+            <div class="phone">${userDetails[i]["phone"]}</div>
+            <div class="address">No 34 adewakeli street Onisha</div>
+            <div class="btnOp"><button type="button" class="editUser" index="${i}">EDIT</button><button type="button" class="deleteUser" index="${i}">REMOVE</button></div>
+        </div>
+    </div>`
+    }
+    userCard.html(userView);
+    },
+    error:function(error){
+      alert(error.statusText)
+    }
+  })
+  }
+  function removeUser(){
+    let i=$(this).attr("index");
+    $.ajax({
+      type:"delete",
+      url:"http://159.65.21.42:9000/user/"+userDetails[i]["_id"],
+      success:function(response){
+        console.log(response);
+    
+        if(response["success"]){
+          alert(`${response["success"]}`)
+          displayUser();
+        }
+      },
+      error:function(error){
+        console.log(error);
+        alert(error.statusText)
+      }
+    })
+
+  }
+  userCard.on("click",".deleteUser", removeUser)
+
+  // dash board view
+ 
+  
+  function dashboardProduct(){
+    dasboardProductView.html(" <h2>Loading data ..........</h2>")
+    $.ajax({
+      type:"get",
+      url:"http://159.65.21.42:9000/products",
+      success:function(response){
+        productDetailsDashboard=response
+        productDetailsDashboard=productDetailsDashboard.reverse()
+        $(".productItems").text(`${productDetailsDashboard.length}`)
+        let quickView="";
+        for (let i=0; i<20; i++){
+          quickView+=`<tr>
+          <td>${productDetailsDashboard[i]["name"]}</td>
+          <td>$${productDetailsDashboard[i]["price"]}</td>
+          <td>${productDetailsDashboard[i]["category"]}</td>
+          <td>${productDetailsDashboard[i]["quantity"]}</td>
+      </tr>`
+      
+        };
+      
+      dasboardProductView.html(quickView);
+      },
+      error:function(error){
+        alert(error.statusText)
+      }
+    })
+  }
+  function dashboardUser(){
+    
+    dasboardUserView.html(" <h2>Loading data ..........</h2>")
+    $.ajax({
+      type:"get",
+      url:"http://159.65.21.42:9000/users",
+      success:function(response){
+        userDetails=response
+        userDetails=userDetails.reverse()
+        $(".userNo").text(`${userDetails.length}`)
+        let userView="";
+        for(let i=0;i<userDetails.length;i++){
+          userView+=` <tr>
+          <td width="60px">
+              <div class="imgBx"><img src="images/profile.jpg" alt=""></div>
+          </td>
+          <td>
+              <h4>${userDetails[i]["name"]}<br> <span>${userDetails[i]["email"]}</span></h4>
+          </td>
+      </tr>
+           <div class="user-card">
+          <div class="img-container">
+              <img src="images/profile.jpg" alt="">
+          </div>`
+      }
+      dasboardUserView.html(userView);
+      },
+      error:function(error){
+        alert(error.statusText)
+      }
+    })
+  }
 
